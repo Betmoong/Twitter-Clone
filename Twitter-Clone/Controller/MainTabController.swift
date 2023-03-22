0c3be3b1804e47bd1809d0a091d1ee7970ae166b
Betmoong <rdev99@naver.com>
2023-03-22 16:00:01 +0900

    [INIT]

---
+++ Twitter-Clone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/03/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
