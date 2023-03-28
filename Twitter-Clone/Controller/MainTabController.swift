352478ae94c806fbe3359733d164e19bad8d1908
Betmoong <rdev99@naver.com>
2023-03-28 21:06:42 +0900

    [FEAT] 사용자 로그인 유지 기능 구현

--- Twitter-Clone/Controller/MainTabController.swift
+++ Twitter-Clone/Controller/MainTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
 
@@ -24,10 +25,34 @@ class MainTabController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemGray5
-        configureViewController()
-        configureUI()
+        //logUserOut()
+        view.backgroundColor = .twitterBlue
+        authenticateUserAndConfigureUI()
+    }
+    
+    // MARK: - API
+    
+    // 사용자가 로그인이 되어있는지 확인하는 메소드
+    func authenticateUserAndConfigureUI() {
+        // 로그인이 안 되어 있는 경우
+        if Auth.auth().currentUser == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+        } else {    // 로그인이 되어 있는 경우
+            configureViewController()
+            configureUI()
+        }
+    }
+    
+    func logUserOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("DEBUG: Failed to sign out with error \(error.localizedDescription)")
+        }
     }
     
     // MARK: - Selectors
