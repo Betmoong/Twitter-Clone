26457ef44be6097d187e54bf4a51c14771cf0cf2
Betmoong <rdev99@naver.com>
2023-04-01 23:16:25 +0900

    [FEAT] UploadTweetController에 profileImageView 구현

--- Twitter-Clone/Controller/UploadTweetController.swift
+++ Twitter-Clone/Controller/UploadTweetController.swift
@@ -11,6 +11,8 @@ class UploadTweetController: UIViewController {
     
     // MARK: - Properties
     
+    private let user: User
+    
     private lazy var actionButton: UIButton = {
         let button = UIButton(type: .system)
         button.backgroundColor = .twitterBlue
@@ -27,11 +29,32 @@ class UploadTweetController: UIViewController {
         return button
     }()
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
     // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        
+        print("DEBUG: User is \(user.username)")
     }
     
     // MARK: - Selectors
@@ -50,7 +73,15 @@ class UploadTweetController: UIViewController {
     
     func configureUI() {
         view.backgroundColor = .white
-
+        configureNavigationBar()
+        
+        view.addSubview(profileImageView)
+        profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 16)
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
+    }
+    
+    func configureNavigationBar() {
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = .white
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
@@ -58,6 +89,5 @@ class UploadTweetController: UIViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleCancel))
         navigationItem.rightBarButtonItem = UIBarButtonItem(customView: actionButton)
-      
     }
 }
