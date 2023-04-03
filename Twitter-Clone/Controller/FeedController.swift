3a8b14e0ab1978706dafa08a589b2fff86873a3a
Betmoong <rdev99@naver.com>
2023-04-03 19:48:10 +0900

    [FEAT] Showing Profile Controller

--- Twitter-Clone/Controller/FeedController.swift
+++ Twitter-Clone/Controller/FeedController.swift
@@ -76,9 +76,14 @@ extension FeedController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifire, for: indexPath) as! TweetCell
         
+        cell.delegate = self
         cell.tweet = tweets[indexPath.row]
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
 }
 
@@ -87,5 +92,14 @@ extension FeedController {
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 120)
+    }
+}
+
+// MARK: - TweetCellDelegate
+
+extension FeedController: TweetCellDelegate {
+    func handleProfileImageTapped() {
+        let controller = ProfileController(collectionViewLayout: UICollectionViewFlowLayout())
+        navigationController?.pushViewController(controller, animated: true)
     }
 }
---
+++ Twitter-Clone/Controller/ProfileController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileController.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/04/03.
+//
+
+import UIKit
+
+class ProfileController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .red
+    }
+}
