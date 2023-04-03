e016637dd8b05d2bcc856685855e82a56dde34b2
Betmoong <rdev99@naver.com>
2023-04-03 13:45:45 +0900

    [FEAT] Setup CollectionView For Feed

---
+++ Twitter-Clone/View/TweetCell.swift
@@ -0,0 +1,21 @@
+//
+//  TweetCell.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/04/03.
+//
+
+import UIKit
+
+class TweetCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
