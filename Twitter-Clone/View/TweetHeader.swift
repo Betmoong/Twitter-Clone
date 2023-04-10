3a6f2dcc3d6e000856f75e5d67b2ea7bdf4d9beb
Betmoong <rdev99@naver.com>
2023-04-10 22:39:09 +0900

    [FEAT] Tweet Display Controller Setup

---
+++ Twitter-Clone/View/TweetHeader.swift
@@ -0,0 +1,25 @@
+//
+//  TweetHeader.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/04/10.
+//
+
+import UIKit
+
+class TweetHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
