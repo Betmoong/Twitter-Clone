ba7837e25fbc33a3824dcec7fa9a46a811952644
Betmoong <rdev99@naver.com>
2023-04-10 20:46:29 +0900

    [FEAT] Following User

--- Twitter-Clone/API/UserService.swift
+++ Twitter-Clone/API/UserService.swift
@@ -10,10 +10,10 @@ import Firebase
 struct UserService {
     static let shared = UserService()
     
-    func fetchUser(uid: String, completion: @escaping(User) -> Void) {        
+    func fetchUser(uid: String, completion: @escaping(User) -> Void) {
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
-                        
+            
             let user = User(uid: uid, dictionary: dictionary)
             completion(user)
         }
@@ -27,6 +27,14 @@ struct UserService {
             let user = User(uid: uid, dictionary: dictionary)
             users.append(user)
             completion(users)
+        }
+    }
+    
+    func followUser(uid: String, completion: @escaping(Error?, DatabaseReference) -> Void) {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        
+        REF_USER_FOLLOWING.child(currentUid).updateChildValues([uid: 1]) { err, ref  in
+            REF_USER_FOLLOWERS.child(uid).updateChildValues([currentUid: 1], withCompletionBlock: completion)
         }
     }
 }
