927b0ac33939fbe4bfd40b36e9fb2606f8b19763
Betmoong <rdev99@naver.com>
2023-04-11 14:04:11 +0900

    [FEAT] Getting Started With Replies

---
+++ Twitter-Clone/ViewModel/UploadTweetViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  UploadTweetViewModel.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/04/11.
+//
+
+import UIKit
+
+enum UploadTweetConfiguration {
+    case tweet
+    case reply(Tweet)
+}
+
+struct UploadTweetViewModel {
+    
+    let actionButtonTitle: String
+    let placeholderText: String
+    var shouldShowReplyLabel: Bool
+    var replyText: String?
+    
+    init(config: UploadTweetConfiguration) {
+        switch config {
+        case .tweet:
+            actionButtonTitle = "Tweet"
+            placeholderText = "What's happening"
+            shouldShowReplyLabel = false
+        case .reply(let tweet):
+            actionButtonTitle = "Reply"
+            placeholderText = "Tweet your reply"
+            shouldShowReplyLabel = true
+            replyText = "Replying to @\(tweet.user.username)"
+        }
+    }
+}
