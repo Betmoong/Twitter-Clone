2aaf11e6fed432b9af9c16e9ef62d8f36d9a868e
Betmoong <rdev99@naver.com>
2023-04-13 15:58:36 +0900

    [FEAT] Data Model

---
+++ Twitter-Clone/View/Profile/EditProfileCell.swift
@@ -0,0 +1,26 @@
+//
+//  EditProfileCell.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/04/12.
+//
+
+import UIKit
+
+class EditProfileCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+}
---
+++ Twitter-Clone/ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  EditProfileViewModel.swift
+//  Twitter-Clone
+//
+//  Created by Rocky on 2023/04/12.
+//
+
+import Foundation
+
+enum EditProfileOptions: Int, CaseIterable {
+    case fullname
+    case username
+    case bio
+    
+    var description: String {
+        switch self {
+        case .username: return "Username"
+        case .fullname: return "Name"
+        case .bio: return "Bio"
+        }
+    }
+}
