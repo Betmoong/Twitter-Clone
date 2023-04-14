7f2393948c612dfde63f519a0959377806517672
Betmoong <rdev99@naver.com>
2023-04-14 17:07:03 +0900

    [FEAT] 탭바의 Index에 따른 액션 버튼 변경(메세지)

--- Twitter-Clone/Controller/SearchController.swift
+++ Twitter-Clone/Controller/SearchController.swift
@@ -1,5 +1,5 @@
 //
-//  ExploreController.swift
+//  SearchController.swift
 //  Twitter-Clone
 //
 //  Created by Rocky on 2023/03/22.
@@ -9,9 +9,16 @@ import UIKit
 
 private let reuseIdentifier = "UserCell"
 
-class ExploreController: UITableViewController {
+enum SearchControllerConfiguration {
+    case messages
+    case userSearch
+}
+
+class SearchController: UITableViewController {
     
     // MARK: - Properties
+    
+    private let config: SearchControllerConfiguration
     
     private var users = [User]() {
         didSet { tableView.reloadData() }
@@ -28,6 +35,15 @@ class ExploreController: UITableViewController {
     private let searchController = UISearchController(searchResultsController: nil)
     
     // MARK: - Lifecycle
+    
+    init (config: SearchControllerConfiguration) {
+        self.config = config
+        super.init(style: .plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,15 +66,26 @@ class ExploreController: UITableViewController {
         }
     }
     
+    // MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: - Helpers
     
     func configureUI() {
         view.backgroundColor = . white
-        navigationItem.title = "Explore"
+        navigationItem.title = config == .messages ? "New Message" : "Search"
         
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 60
         tableView.separatorStyle = .none
+        
+        if config == .messages {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
+                                                               target: self, action: #selector(handleDismissal))
+        }
     }
     
     func configureSearchController() {
@@ -73,7 +100,7 @@ class ExploreController: UITableViewController {
 
 // MARK: - UITableViewDelegate/DataSource
 
-extension ExploreController {
+extension SearchController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return inSearchMode ? filterUsers.count : users.count
     }
@@ -94,7 +121,7 @@ extension ExploreController {
 
 // MARK: - UISearchResultsUpdating
 
-extension ExploreController: UISearchResultsUpdating {
+extension SearchController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         guard let searchText = searchController.searchBar.text?.lowercased() else { return }
         
